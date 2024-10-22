97f2337ed760c42020f6f4a3bfe30ac3894cc842
Samapa1 <saija.m.pajari@gmail.com>
2024-10-22 16:29:41 +0300

    restricting adding and deleting books only to admins

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -18,8 +18,24 @@ router.put('/:id', async (req, res) => {
 })
 
 router.post('/', tokenExtractor, async (req, res) => {
-  const book = await Book.create({...req.body})
-  res.json(book)
+  if (req.user.admin === "true") {
+    const book = await Book.create({...req.body})
+    res.json(book)
+  }
+  else {
+    res.status(403).end()
+  }
+  })
+
+router.delete('/:id', tokenExtractor, async (req, res) => {
+  if (req.user.admin === "true") {
+    const book = await Book.findByPk(req.params.id)
+    await book.destroy()
+    res.status(204).end()
+  }
+  else {
+    res.status(403).end()
+  }
   })
 
 module.exports = router
