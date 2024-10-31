05bfad5450a03da4c62deb8fd8749e27a6599d0c
Samapa1 <saija.m.pajari@gmail.com>
2024-10-31 10:39:32 +0200

    fixing reservations(backend)

--- backend/controllers/reservations.js
+++ backend/controllers/reservations.js
@@ -36,7 +36,7 @@ router.post('/', tokenExtractor, async (req, res) => {
         ]
     })
 
-    if (book.numberOfBooks > book.loans.length) {
+    if (book.numberOfBooks > (book.loans.length + book.reservations.length)) {
         return res.status(400).json({error: 'Available books can not be reserved.'})
     }
    
