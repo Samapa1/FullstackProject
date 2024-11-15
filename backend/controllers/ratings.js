5cf99660c916e8cee230c8a52564f5ff03768a80
Samapa1 <saija.m.pajari@gmail.com>
2024-11-15 14:31:37 +0200

    developing rating (backend)

--- backend/controllers/ratings.js
+++ backend/controllers/ratings.js
@@ -14,56 +14,42 @@ router.get('/', tokenExtractor, async (req, res) => {
 })
 
 router.post('/', tokenExtractor, async (req, res) => {
-    console.log("post rating")
     if (req.body.userId !== req.user.id) {
         return res.status(403).end()
     }
+
+    const updateBook = async () => {
+        const book = await Book.findByPk(req.body.bookId)
+        const allRatings = await Rating.findAll({
+            where: {
+                bookId: req.body.bookId}
+        })
+        let sum = 0
+        allRatings.forEach(rating => sum += rating.stars)
+        const average = sum/(allRatings.length)
+        book.rating = average
+        await book.save()
+    }
+
     const rated = await Rating.findOne({ 
+
         where: {
             userId: req.user.id,
             bookId: req.body.bookId
         },
     })
+
     if (rated) {
-        return res.status(403).json({error: 'You have already rated the book!'})
+        const stars = req.body.stars
+        rated.stars = stars
+        await rated.save()
+        updateBook()
+        return res.status(200).json(rated)
+        
     }
     const newRating = await Rating.create({...req.body})
-    const book = await Book.findByPk(req.body.bookId)
-    const allRatings = await Rating.findAll({
-        where: {
-            bookId: req.body.bookId}
-    })
-    let sum = 0
-    allRatings.forEach(rating => sum += rating.stars)
-    const average = sum/(allRatings.length)
-    book.rating = average
-    await book.save()
-
+    updateBook()
     return res.json(newRating)
-
-})
-
-router.post('/:id', tokenExtractor, async (req, res) => {
-    const rating = await Rating.findByPk(req.params.id) 
-    console.log("updateRating")
-    if (rating.userId !== req.user.id) {
-        return res.status(403).end()
-    }
-    rating.stars = req.body.stars
-    await rating.save()
-
-    const book = await Book.findByPk(rating.bookId)
-    const allRatings = await Rating.findAll({
-        where: {
-            bookId: rating.bookId}
-    })
-    let sum = 0
-    allRatings.forEach(rating => sum += rating.stars)
-    const average = sum/(allRatings.length)
-    book.rating = average
-    await book.save()
-
-    return res.status(200).json(rating)
 
 })
 
