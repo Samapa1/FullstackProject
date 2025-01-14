a2391db7ef25a925793b5e56b9857133a1e2d7e3
Samapa1 <saija.m.pajari@gmail.com>
2025-01-14 15:16:57 +0200

    developing workflow

--- tests/pipeline.spec.js
+++ tests/pipeline.spec.js
@@ -18,11 +18,11 @@ describe('Book app', () => {
     await expect(page.getByText('Welcome to the book app')).toBeVisible()
   })
 
-  test('booklist can be opened', async ({ page }) => {
-    await expect(page.getByText('Welcome to the book app')).toBeVisible()
-    await page.getByText('books', { exact: true }).click()
-    await page.getByText('The Stranger by Albert Camus').waitFor()
-  })
+  // test('booklist can be opened', async ({ page }) => {
+  //   await expect(page.getByText('Welcome to the book app')).toBeVisible()
+  //   await page.getByText('books', { exact: true }).click()
+  //   await page.getByText('The Stranger by Albert Camus').waitFor()
+  // })
 
   // test('login fails with wrong credentials', async ({ page }) => {
   //   await page.getByText('log in').click()
@@ -33,13 +33,15 @@ describe('Book app', () => {
   //   await expect(page.getByText('invalid username or password')).toBeVisible()
   // })
 
-  // test('login succeeds with right credentials', async ({ page }) => {
-  //   await page.getByText('log in').click()
-  //   await expect(page.getByText('Log in to application')).toBeVisible()
-  //   await page.getByRole('textbox').first().fill('testuser')
-  //   await page.getByRole('textbox').last().fill('secret10')
-  //   await page.getByRole('button', { name: 'log in' }).click()
-  //   await page.getByText('testuser logged in').waitFor()
-  //   })
+  test('login succeeds with right credentials', async ({ page }) => {
+    await page.getByText('log in').click()
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await page.getByRole('textbox').first().fill('testuser')
+    await page.getByRole('textbox').last().fill('secret10')
+    await page.getByRole('button', { name: 'log in' }).click()
+    await page.getByText('testuser logged in').waitFor()
+    await page.getByText('my page').waitFor()
+
+    })
 
 })
