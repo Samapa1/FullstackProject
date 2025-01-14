9e9a41d9b965fb3c501403b454fa3b10c006d301
Samapa1 <saija.m.pajari@gmail.com>
2025-01-14 13:57:59 +0200

    developing workflow

--- tests/bookApp.spec.js
+++ tests/bookApp.spec.js
@@ -34,6 +34,7 @@ describe('Book app', () => {
       await page.getByRole('textbox').first().fill('testuser')
       await page.getByRole('textbox').last().fill('secret10')
       await page.getByRole('button', { name: 'log in' }).click()
+      await expect(page.getByText('testuser logged in')).toBeVisible()
       // await page.getByText('Here you can borrow books and return your loans.').waitFor()
       // await expect(page.getByText('Here you can borrow books and return your loans.')).toBeVisible()
       // await expect(page.getByText('testuser logged in')).toBeVisible()
