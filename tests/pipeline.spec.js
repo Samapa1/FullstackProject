426ca52254e0eb4845591b291276b1b02fbb9f56
Samapa1 <saija.m.pajari@gmail.com>
2025-01-14 15:02:47 +0200

    developing workflow

---
+++ tests/pipeline.spec.js
@@ -0,0 +1,45 @@
+const { test, expect, describe, beforeEach } = require('@playwright/test')
+
+describe('Book app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Testuser',
+        email: 'testuser@gmal.com',
+        username: 'testuser',
+        password: 'secret10'
+      }
+    })
+    await page.goto('http://localhost:5173')
+  })
+
+  test('front page can be opened', async ({ page }) => {
+    await expect(page.getByText('Welcome to the book app')).toBeVisible()
+  })
+
+  test('booklist can be opened', async ({ page }) => {
+    await expect(page.getByText('Welcome to the book app')).toBeVisible()
+    await page.getByText('books', { exact: true }).click()
+    await page.getByText('The Stranger by Albert Camus').waitFor()
+  })
+
+  test('login fails with wrong credentials', async ({ page }) => {
+    await page.getByText('log in').click()
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await page.getByRole('textbox').first().fill('testuser')
+    await page.getByRole('textbox').last().fill('secret1')
+    await page.getByRole('button', { name: 'log in' }).click()
+    await expect(page.getByText('invalid username or password')).toBeVisible()
+  })
+
+  test('login succeeds with right credentials', async ({ page }) => {
+    await page.getByText('log in').click()
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await page.getByRole('textbox').first().fill('testuser')
+    await page.getByRole('textbox').last().fill('secret10')
+    await page.getByRole('button', { name: 'log in' }).click()
+    await page.getByText('testuser logged in').waitFor()
+    })
+
+})
